424806aaa6663ee1efcc84f8e910c1a2f96d4f1b
Brian Han <brianhan87usa@gmail.com>
2013-08-27 14:45:52 -0400

    Finish book.rb

--- 07_book_titles/book.rb
+++ 07_book_titles/book.rb
@@ -1,17 +1,19 @@
 class Book
-	LITTLE_WORDS = %w{ and }
+
+	LITTLE_WORDS = %w{ the and a an in of }
+
 	attr_reader :title
 
 	def title=( book_title )
 		@title = book_title
 	end
 
-
 	def title
 		words = @title.split(" ")
-		words.map { |word| word.capitalize! }
+		words.map do |word| 
+			word.capitalize! unless LITTLE_WORDS.include?(word)
+		end
+		words.first.capitalize!
 		words.join(" ")
 	end
-
-
 end
